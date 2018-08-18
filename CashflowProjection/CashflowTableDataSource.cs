eb8c318b2f0d835ba5df6810cf5e21f8d183277f
Aaron Lebovitz <aaron.lebovitz@adaptation.capital>
2018-08-17 23:55:36 -0500

    bug fix in table sort

--- CashflowProjection/CashflowTableDataSource.cs
+++ CashflowProjection/CashflowTableDataSource.cs
@@ -196,18 +196,18 @@ namespace CashflowProjection
 
         public override void SortDescriptorsChanged(NSTableView tableView, NSSortDescriptor[] oldDescriptors)
         {
-            // Sort the data
-            if (oldDescriptors.Length > 0)
-            {
-                // Update sort
-                Sort(oldDescriptors[0].Key, oldDescriptors[0].Ascending);
-            }
-            else
-            {
+            //// Sort the data
+            //if (oldDescriptors.Length > 0)
+            //{
+            //    // Update sort
+            //    Sort(oldDescriptors[0].Key, oldDescriptors[0].Ascending);
+            //}
+            //else
+            //{
                 // Grab current descriptors and update sort
                 NSSortDescriptor[] tbSort = tableView.SortDescriptors;
                 Sort(tbSort[0].Key, tbSort[0].Ascending);
-            }
+            //}
 
             // Refresh table
             tableView.ReloadData();
